ad7daf779f55facc9dc8dcc356db0812b63ed75f
AvisP <avishekpaul31@gmail.com>
2018-01-28 21:45:52 -0500

    Added Section8

---
+++ Section8/Artificial_Neural_Networks/ann_avishek.R
@@ -0,0 +1,78 @@
+# ANN
+
+# Importing the dataset
+dataset = read.csv('Churn_Modelling.csv')
+dataset = dataset[4:14]
+
+#Encoding categorical variables as factors
+dataset$Geography = as.numeric(factor( dataset$Geography,
+						   levels = c('France','Spain','Germany'),
+						   labels = c(1,2,3)))
+dataset$Gender = as.numeric(factor( dataset$Gender,
+						   levels = c('Female','Male'),
+						   labels = c(1,2)))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Exited, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-11] = scale(training_set[-11])
+test_set[-11] = scale(test_set[-11])
+
+# Fitting ANN to the Training set
+# install.packages('h2o')
+library(h2o)
+h2o.init(nthreads = -1)
+classifier = h2o.deeplearning(y = 'Exited',
+					training_frame = as.h2o(training_set),
+					activation = 'Rectifier',
+					hidden = c(6,6),
+					epochs = 100,
+					train_samples_per_iteration = -2)
+
+# Predicting the Test set results
+prob_pred = h2o.predict(classifier, newdata = as.h2o(test_set[-11]))
+y_pred = (prob_pred > 0.5)
+y_pred = as.vector(y_pred)
+
+# Making the Confusion Matrix
+cm = table(test_set[, 11], y_pred)
+accuracy = (cm[1,1] + cm[2,2]) / (cm[1,1] + cm[1,2] + cm[2,1] + cm[2,2])
+
+h2o.shutdown()
+
+# Visualising the Training set results
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3],
+     main = 'SVM (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3], main = 'SVM (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
